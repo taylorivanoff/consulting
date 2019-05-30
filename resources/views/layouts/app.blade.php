c582301e97b4d23400c8422e25db009e94445b69
Taylor Ivanoff <taylorivanoff@gmail.com>
2019-05-30 16:28:06 +1000

    Fixed hamburger.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,11 +25,11 @@
             <div class="container">
                 <a class="navbar-brand text-lowercase mr-lg-5" style="line-height: 0.94;" href="/">Taylor<br> Ivanoff<br> Consulting</a>
 
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <!--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <!--<div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item mr-3">
                             <a class="nav-link" href="/consulting">Consulting</a>

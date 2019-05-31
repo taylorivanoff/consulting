826f80efd0b16d3dad4409383f494670b097e8b4
Taylor Ivanoff <taylorivanoff@gmail.com>
2019-05-31 22:23:11 +1000

    Modified email response.

--- resources/views/emails/registered.blade.php
+++ resources/views/emails/registered.blade.php
@@ -1,9 +1,7 @@
 @component('mail::message')
-##Dear designer,
+Thank you for registering your interest in the BeSpoke™ program.
 
-Thank you for registering your interest in receiving our digital strategy precedent for portfolio-based websites.
-
-The guide will be attached and sent to this e-mail address upon it's release in June, 2019.
+You'll be notified with more information about the program by the 14th of July.
 
 Kind regards,<br>
 Taylor Ivanoff
